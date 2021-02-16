e1ace1d615a9ee6428b4b642463a40559869fa0d
Vyacheslav Borisov <slava2bb@gmail.com>
2021-02-17 00:54:34 +0300

    Added data validation

--- app/src/main/java/com/kushnir/elfc/activity/LangsListActivity.java
+++ app/src/main/java/com/kushnir/elfc/activity/LangsListActivity.java
@@ -2,7 +2,9 @@ package com.kushnir.elfc.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.MutableLiveData;
@@ -36,10 +38,21 @@ public class LangsListActivity extends AppCompatActivity {
         langRecyclerView.setAdapter(adapter);
 
         MutableLiveData<String> lang = new MutableLiveData<>();
-        lang.observe(this, s -> langs.add(new LangsListItem(s, 0, v -> {
-            Intent intent = new Intent(this, SubjectsListActivity.class);
-            startActivity(intent);
-        })));
+        lang.observe(this, s -> {
+            if(s.isEmpty()) {
+                Toast.makeText(
+                        this,
+                        getResources().getText(R.string.empty_lang_input_toast),
+                        Toast.LENGTH_LONG).show();
+            }
+            else {
+                langs.add(new LangsListItem(s, 0, v -> {
+                    Intent intent = new Intent(this, SubjectsListActivity.class);
+                    intent.putExtra("lang", s);
+                    startActivity(intent);
+                }));
+            }
+        });
 
         addButton.setOnClickListener(v -> {
             AddLangDialogFragment dialog = new AddLangDialogFragment(lang);
--- app/src/main/java/com/kushnir/elfc/activity/SubjectsListActivity.java
+++ app/src/main/java/com/kushnir/elfc/activity/SubjectsListActivity.java
@@ -1,8 +1,11 @@
 package com.kushnir.elfc.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.MutableLiveData;
@@ -30,16 +33,28 @@ public class SubjectsListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subjects_list);
+        Intent intent = getIntent();
+
+        TextView langTextView = findViewById(R.id.subjects_list_lang_text_view);
+        langTextView.setText(intent.getStringExtra("lang"));
 
         subjects = new ArrayList<>();
-        subjects.add(new SubjectsListItem("Food", 5, v -> Log.i("ELFC", "Food")));
 
         // Загрузка из бд
 
         MutableLiveData<String> subject = new MutableLiveData<>();
-        subject.observe(this, s -> subjects.add(new SubjectsListItem(s, 0, v -> {
-            Log.i("App", "Subject: " + s);
-        })));
+        subject.observe(this, s -> {
+            if (s.isEmpty()) {
+                Toast.makeText(
+                        this,
+                        getResources().getText(R.string.empty_subject_input_toast),
+                        Toast.LENGTH_LONG).show();
+            } else {
+                subjects.add(new SubjectsListItem(s, 0, v -> {
+                    Log.i("App", "Subject: " + s);
+                }));
+            }
+        });
 
         addButton = findViewById(R.id.add_subject_button);
         addButton.setOnClickListener(v -> {
